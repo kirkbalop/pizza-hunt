f9579382a3c049dc2d676a49ee4bbbc141ece268
Kirk <kirkbalop@gmail.com>
2021-04-06 00:07:52 -0700

    added user models and add-pizza functionality

---
+++ models/pizza.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const pizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// Create model using pizza schema
+const Pizza = model('Pizza', pizzaSchema);
+
+module.exports = Pizza;

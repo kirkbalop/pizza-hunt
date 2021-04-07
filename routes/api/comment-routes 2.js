77e63cd2654cfc970e24bdb642614a44ad3566ad
Kirk <kirkbalop@gmail.com>
2021-04-06 19:14:30 -0700

    fixed gitignore

---
+++ routes/api/comment-routes 2.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controllers');
+const { route } = require('./pizza-routes');
+
+// api/comments/:pizzaid
+router
+    .route('/:pizzaId')
+    .post(addComment);
+
+// api/comments/<pizzaId>/<commentId>
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
+
+module.exports = router;
